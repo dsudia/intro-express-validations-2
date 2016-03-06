8f292804e9f438c14f11055149affda57acade90
David Sudia <dsudia@gmail.com>
2016-03-06 16:20:39 -0700

    add logic for rendering show page using database, including success message

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,15 +3,41 @@ var router = express.Router();
 var pg = require('pg');
 var knex = require('../db/knex');
 
+function validSubmit(req, res, next) {
+  if (req.body.name === '' && req.body.hobby === '') {
+    req.flash('danger', 'You must enter a name.\nYou must enter a hobby.');
+    res.redirect('/');
+  } else if (req.body.name === '' && req.body.hobby !== '') {
+    req.flash('danger', 'You must enter a name.');
+    res.redirect('/');
+  } else if (req.body.name !== '' && req.body.hobby === '') {
+    req.flash('danger', 'You must enter a hobby.');
+    res.redirect('/');
+  } else {
+    return next();
+  }
+}
+
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.post('/', function(req, res, next) {
+router.get('/show', function(req, res, next) {
+  knex('people').select().then(function(data) {
+    res.render('show', {messages: req.flash(success), people: data});
+  });
+});
+
+router.post('/', validSubmit, function(req, res, next) {
   var person = req.body;
   knex('people').insert({
     name: person.name,
     hobby: person.hobby
+  }).catch(function(err) {
+    console.log(err);
+  }).then(function(data) {
+    req.flash('success', 'The person was saved successfully!');
+    res.redirect('/show');
   });
 });
 
