7edf6ab690f36917503449ebec465b65a4a67480
David Sudia <dsudia@gmail.com>
2016-03-06 15:53:39 -0700

    add unique quality to name column

--- migrations/20160306154635_create_people_table.js
+++ migrations/20160306154635_create_people_table.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('people', function(table) {
     table.increments();
     table.string('name');
+    column.unique('name');
     table.string('hobby');
   });
 };
