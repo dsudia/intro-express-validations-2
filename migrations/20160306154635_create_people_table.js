bed1b827b87827cca214cb079d4ca571c25da20b
David Sudia <dsudia@gmail.com>
2016-03-06 15:49:41 -0700

    write migration to create people table

--- migrations/20160306154635_create_people_table.js
+++ migrations/20160306154635_create_people_table.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  knex.schema.createTable('people', function(table) {
+    table.increments();
+    table.string('name');
+    table.string('hobby');
+  });
 };
 
 exports.down = function(knex, Promise) {
-
+  knex.schema.dropTable('people');
 };
